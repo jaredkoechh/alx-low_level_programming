56dfa30397c475fefaf229cc6b4ff106b7deb748
jaredkoechh <jaredkoechh@gmail.com>
2022-07-24 18:41:12 +0000

    Task 0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - prints the no. of arg
+ *@argc: argument count
+ *@argv: array of pointers to CLI argument
+ *Return: 0
+ **/
+
+int main(int argc, char *argv[])
+{
+printf("argc %d", argc);
+return (0);
+}
