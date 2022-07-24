3b767528f16da0b33fa685f28ce15db2e707ccda
jaredkoechh <jaredkoechh@gmail.com>
2022-07-24 18:46:33 +0000

    Task 0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- *main - prints a name followed by a new line 
- *@argc: arguement count
- *@argv: array of pointers to argc
- *Return: 0
+ * main - prints a name followed by a new line 
+ * @argc: arguement count
+ * @argv: array of pointers to argc
+ * Return: 0
  **/
 
 int main(int argc, char *argv[])
 {
-printf("%s\n", argv[argc - 1]);
-return (0);
+
+	printf("%s\n", argv[argc - 1]);
+	return (0);
 }
